dbfa3220036f14b42bf12a198fc24c38b1b1cbf1
Admin <Admin@Admin-PC>
2022-01-26 23:40:31 +0530

    my first commit

---
+++ temp/src/addition/Addition.java
@@ -0,0 +1,14 @@
+package addition;
+
+public class Addition 
+{
+     public static void main(String[] args) 
+     {   
+    	 System.out.println("Enter two numbers : ");
+    	 int first=250;
+    	 int second=500;
+    	 System.out.println(first+" "+second);
+    	 int sum=first+second;
+    	 System.out.println("The Sum is "+sum);
+     }
+}
