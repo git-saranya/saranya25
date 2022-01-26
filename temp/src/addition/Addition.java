1549f7aa38b7aaa6b32cccabeedd73204b5aa1f8
git-saranya <98472753+git-saranya@users.noreply.github.com>
2022-01-26 23:48:05 +0530

    Update Addition.java

--- temp/src/addition/Addition.java
+++ temp/src/addition/Addition.java
@@ -1,5 +1,5 @@
 package addition;
-
+import java.util.scanner;
 public class Addition 
 {
      public static void main(String[] args) 
@@ -7,6 +7,11 @@ public class Addition
     	 System.out.println("Enter two numbers : ");
     	 int first=250;
     	 int second=500;
+      Scanner scan=new Scanner(System.in);
+      System.out.println("Enter your First Number: ");
+      first=scan.NextInt();
+      System.out.println("Enter your Second Number: ");
+      second=scan.NextInt();          
     	 System.out.println(first+" "+second);
     	 int sum=first+second;
     	 System.out.println("The Sum is "+sum);
